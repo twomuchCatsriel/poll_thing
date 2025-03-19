31ad804181059575dee7db884fc988dbe309226f
catsriel <danbro07@imkatta.no>
2025-03-19 10:15:09 +0100

    Make the function work
    IT FRIGGING WORKS

--- js/new_poll.js
+++ js/new_poll.js
@@ -5,7 +5,6 @@ const resultsArea = document.getElementById("results");
 // Options 
 const options = document.getElementsByClassName("options");
 
-
 // results 
 const percentYes = document.getElementById("percentYes");
 const percentNo = document.getElementById("percentNo");
@@ -27,31 +26,32 @@ function showResults()
 }
 
 function addPointsAndSubmit(ans){ // this is also mostly a placeholder 
-    if(ans === "Ja"){
-        points[0] += 1;
+    let idNum;
+    if(ans === options[0].innerHTML){
+        idNum = "A";
     } 
-    else if (ans === "Nei"){
-        points[1] += 1;
+    else if (ans === options[1].innerHTML){
+        idNum = "B";
     }
-    else if (ans === "Kanskje"){
-        points[2] += 1;
+    else if (ans === options[2].innerHTML){
+        idNum = "C";
     }
 
-    fetch("http://192.168.167.15:3000/votes", {
+    let idObj = {
+        answerId: idNum
+    }
+
+    console.log(JSON.stringify(idObj))
+
+    fetch("http://192.168.167.15:3000/vote", {
         method: "POST",
         headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-type": "application/json; charset=UTF-8"
         },
-        body: ans.JSON
+        body: JSON.stringify(idObj)
     })
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
-
-    let total = (points[0] + points[1] + points[2]);
-    percentYes.innerHTML = Math.floor((points[0]/total) * 100);
-    percentNo.innerHTML = Math.floor((points[1]/total) * 100);
-    percentMaybe.innerHTML = Math.floor((points[2]/total) * 100);
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
 
 for(let i = 0; i < options.length; i++){ // register clicks 
@@ -62,3 +62,9 @@ for(let i = 0; i < options.length; i++){ // register clicks
         console.log(answer); //debug
     })
 }
+
+// bleh
+/*  let total = (points[0] + points[1] + points[2]);
+    percentYes.innerHTML = Math.floor((points[0]/total) * 100);
+    percentNo.innerHTML = Math.floor((points[1]/total) * 100);
+    percentMaybe.innerHTML = Math.floor((points[2]/total) * 100); */
